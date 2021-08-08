7442ef8977e0af7274ee71c1f632ac7d305a36c1
melfilin <olga.volodina.web@gmail.com>
2021-08-08 17:39:57 +0300

    create mobile menu

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,15 @@
 const modal = document.querySelector('.modal__dialog');
 const modalOverlay = document.querySelector('.modal__overlay');
 const navbarButton = document.querySelector('.navbar-button');
+const navbarButtonMobile = document.querySelector('.navbar-button__mobile');
 const modalCloseBtn = document.querySelectorAll('.modal__close');
 
 modalOverlay.addEventListener('click', modalClose);
 navbarButton.addEventListener('click', modalOpen);
+navbarButtonMobile.addEventListener('click', () => {
+  closeMobile();
+  modalOpen();
+});
 modalCloseBtn.forEach((close) => close.addEventListener('click', modalClose));
 document.addEventListener('keydown', clickDocument);
 
